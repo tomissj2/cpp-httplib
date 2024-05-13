38c59825a90a574f3080c549e98f82614f1f22b0
tomissj2 <kasstamas@gmail.com>
2024-05-13 15:12:18 +0200

    updated client
    added logic to get and use the X-CSRF-TOKEN

--- example/client.cc
+++ example/client.cc
@@ -38,14 +38,34 @@ int main(int argc, char *argv[]) {
   std::string url = "http://" + host + ":" + port;
   httplib::Client client(url);
 
-  auto res = client.Post(endpoint, fileContents, "text/plain");
+  auto res = client.Get("/token");
 
-  if (res.error() == httplib::Error::Success) {
-    std::cout << "Successfully posted the file contents to /upload."
+  if (res && res->status == 200)
+  {
+      std::string response_body = res->body;
+
+      httplib::Headers headers = {
+          {"X-CSRF-TOKEN", response_body} };
+
+      auto res = client.Post(endpoint, headers, fileContents, "text/plain");
+
+      if (res.error() == httplib::Error::Success)
+      {
+          std::cout << "Successfully posted the file contents to " + endpoint
               << std::endl;
-  } else {
-    std::cerr << "Error posting to /upload: " << httplib::to_string(res.error())
+      }
+      else
+      {
+          std::cerr << "Error posting to " + endpoint << httplib::to_string(res.error())
               << std::endl;
+          return - 1;
+      }
+  }
+  else
+  {
+      // Print error message
+      std::cerr << "Error: " << (res ? res->status : -1) << std::endl;
+      return -1;
   }
 
   return 0;
